ebc19c74ac35867588f78a525825e1874fa57a43
Gabriel Duquette <gabrielduquette@gmail.com>
2019-11-30 16:03:21 -0500

    a few stripes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import './index.css';
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const StripesContainer = styled.div`
+`
+
+const Stripe = styled.div`
+  background: ${props => props.color}
+  height: 38px;
+  width: 600px;
+`
 
 function App() {
+  const colors = {
+    'red': '#367da2',
+    'blue': '#e93f36',
+    'black': 'black'
+  }
+
+  const stripeBox = ['red', 'blue', 'black'];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <StripesContainer>
+        {stripeBox.map((stripe, i) => (
+            <Stripe key={i} color={colors[stripeBox[i]]} />
+          )
+        )}
+        </StripesContainer>
+    </Container>
   );
 }
 
-export default App;
+export default App
