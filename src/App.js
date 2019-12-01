e91c533dad015f0a0b70fe9e65479ea9a47ecef6
Gabriel Duquette <gabrielduquette@gmail.com>
2019-12-01 13:24:16 -0500

    get Selects working

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
+import Select from 'react-select'
 import './index.css';
 
 const Container = styled.div`
   height: 100%;
   width: 100%;
   display: flex;
+  flex-direction: row;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
+  margin-top: 20px;
 `
 
-const StripesContainer = styled.div`
+const SelectContainer = styled.div`
+  width: 150px;
+  margin-right: 20px;
 `
+
+const SelectHeader = styled.h5``
+
+const StripesContainer = styled.div``
 
 const Stripe = styled.div`
   background: ${props => props.color}
   height: 38px;
   width: 600px;
 `
-// 7/12(1-1-1-6-1-1-1)/2(ABXAXBA)
-// number of stripes (each may be comprised of one or more units)
-// total number of units
-// units assigned to each stripe, top to bottom
-// how many colors are used
-// color of each stripe, top to bottom
-
-// write a function that takes two numbers as input, x and y
-// x should be less than or equal to y
-// both x and y should be greater than or equal to 1
-// both x and y should be less than or equal to 16
-// it should return a palindromic array of length x that adds up to y
 
 function App() {
   const colors = {
@@ -40,8 +37,30 @@ function App() {
 
   const stripeBox = ['red', 'blue', 'black'];
 
+  const maxStripeCount = 16
+
+  const createOptions = (start, end) => {
+    let options = []
+      for (let i = start; i <= end; i++) {
+        const num = i.toString()
+        options.push({ value: num, label: num })
+    }
+    return options
+  }
+
+  const [stripeCount, setStripeCount] = useState(maxStripeCount)
+  const [stripeOptions, setStripeOptions] = useState(createOptions(1, maxStripeCount))
+  const [magnitude, setMagnitude] = useState(maxStripeCount)
+  const [magnitudeOptions, setMagnitudeOptions] = useState(createOptions(stripeCount, maxStripeCount))
+
   return (
     <Container>
+      <SelectContainer>
+        <SelectHeader>Select stripe count</SelectHeader>
+        <Select options={stripeOptions} onChange={(option) => setMagnitudeOptions(createOptions(option.value, maxStripeCount))} />
+        <SelectHeader>Select magnitude</SelectHeader>
+        <Select options={magnitudeOptions} />
+      </SelectContainer>
       <StripesContainer>
         {stripeBox.map((stripe, i) => (
             <Stripe key={i} color={colors[stripeBox[i]]} />
