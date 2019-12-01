0ee4c3bd8a7a19972c1927ea80883089f6f423c7
Gabriel Duquette <gabrielduquette@gmail.com>
2019-11-30 23:57:17 -0500

    clarify problem statement

--- src/App.js
+++ src/App.js
@@ -18,6 +18,18 @@ const Stripe = styled.div`
   height: 38px;
   width: 600px;
 `
+// 7/12(1-1-1-6-1-1-1)/2(ABXAXBA)
+// number of stripes (each may be comprised of one or more units)
+// total number of units
+// units assigned to each stripe, top to bottom
+// how many colors are used
+// color of each stripe, top to bottom
+
+// write a function that takes two numbers as input, x and y
+// x should be less than or equal to y
+// both x and y should be greater than or equal to 1
+// both x and y should be less than or equal to 16
+// it should return a palindromic array of length x that adds up to y
 
 function App() {
   const colors = {
