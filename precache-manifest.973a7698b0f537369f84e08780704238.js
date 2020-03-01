ec0b0a202c201ceabd2b8e848adeba31926ded99
Gabriel Duquette <gabrielduquette@gmail.com>
2020-03-01 14:45:25 -0500

    Updates

---
+++ precache-manifest.973a7698b0f537369f84e08780704238.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e603aaba23c61e8d5e3b291844af55db",
+    "url": "/stripes/index.html"
+  },
+  {
+    "revision": "20e440d00ddea57e889c",
+    "url": "/stripes/static/css/main.326ad20d.chunk.css"
+  },
+  {
+    "revision": "ba2720cf13e5e70322a2",
+    "url": "/stripes/static/js/2.9dfe9637.chunk.js"
+  },
+  {
+    "revision": "20e440d00ddea57e889c",
+    "url": "/stripes/static/js/main.db7bacb3.chunk.js"
+  },
+  {
+    "revision": "e431579e9abecbaab66d",
+    "url": "/stripes/static/js/runtime-main.cf93b053.js"
+  }
+]);
